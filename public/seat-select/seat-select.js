53dbbec7c0d6150d816c3b4964ba491980ad3f10
myriam mansouri <myriam.mansouri@gmail.com>
2020-07-16 11:37:20 -0400

    Completed flight number verification and error message

--- public/seat-select/seat-select.js
+++ public/seat-select/seat-select.js
@@ -1,89 +1,88 @@
-const flightInput = document.getElementById('flight');
-const seatsDiv = document.getElementById('seats-section');
-const confirmButton = document.getElementById('confirm-button');
+const flightInput = document.getElementById("flight");
+const seatsDiv = document.getElementById("seats-section");
+const confirmButton = document.getElementById("confirm-button");
+const errorMsg = document.getElementById('error');
 
-let selection = '';
+let selection = "";
 
-// check if flight number has correct format SA###
-// passes letter to uppercase to comply with database format
-// and returns flight
-const verifyFlightFormat = (flightNumber) =>{
-    flightNumber = flightNumber.split("").map( el => el.toUpperCase()).join("")
-    if (flightNumber.length === 5 && flightNumber.slice(0, 2) === "SA") {
-        return flightNumber
+const errorMessages = {
+  'do-not-exist': "Flight number doesn't exist",
+  'invalid-format': "Invalid flight format. Should be SA###",
+};
+
+const renderSeats = (seats) => {
+  document.querySelector(".form-container").style.display = "block";
+
+  const alpha = ["A", "B", "C", "D", "E", "F"];
+  for (let r = 1; r < 11; r++) {
+    const row = document.createElement("ol");
+    row.classList.add("row");
+    row.classList.add("fuselage");
+    seatsDiv.appendChild(row);
+    for (let s = 1; s < 7; s++) {
+      const seatNumber = `${r}${alpha[s - 1]}`;
+      const seat = document.createElement("li");
+
+      // Two types of seats to render
+      const seatOccupied = `<li><label class="seat"><span id="${seatNumber}" class="occupied">${seatNumber}</span></label></li>`;
+      const seatAvailable = `<li><label class="seat"><input type="radio" name="seat" value="${seatNumber}" /><span id="${seatNumber}" class="avail">${seatNumber}</span></label></li>`;
+
+      // TODO: render the seat availability based on the data...
+      seat.innerHTML = seatAvailable;
+      row.appendChild(seat);
     }
-}
-const renderSeats = () => {
-    document.querySelector('.form-container').style.display = 'block';
+  }
 
-    const alpha = ['A', 'B', 'C', 'D', 'E', 'F'];
-    for (let r = 1; r < 11; r++) {
-        const row = document.createElement('ol');
-        row.classList.add('row');
-        row.classList.add('fuselage');
-        seatsDiv.appendChild(row);
-        for (let s = 1; s < 7; s++) {
-            const seatNumber = `${r}${alpha[s-1]}`;
-            const seat = document.createElement('li');
-
-            // Two types of seats to render
-            const seatOccupied = `<li><label class="seat"><span id="${seatNumber}" class="occupied">${seatNumber}</span></label></li>`
-            const seatAvailable = `<li><label class="seat"><input type="radio" name="seat" value="${seatNumber}" /><span id="${seatNumber}" class="avail">${seatNumber}</span></label></li>`        
-            
-            // TODO: render the seat availability based on the data...
-            seat.innerHTML = seatAvailable;
-            row.appendChild(seat);
+  let seatMap = document.forms["seats"].elements["seat"];
+  seatMap.forEach((seat) => {
+    seat.onclick = () => {
+      selection = seat.value;
+      seatMap.forEach((x) => {
+        if (x.value !== seat.value) {
+          document.getElementById(x.value).classList.remove("selected");
         }
-    }
-    
-    let seatMap = document.forms['seats'].elements['seat'];
-    seatMap.forEach(seat => {
-        seat.onclick = () => {
-            selection = seat.value;
-            seatMap.forEach(x => {
-                if (x.value !== seat.value) {
-                    document.getElementById(x.value).classList.remove('selected');
-                }
-            })
-            document.getElementById(seat.value).classList.add('selected');
-            document.getElementById('seat-number').innerText = `(${selection})`;
-            confirmButton.disabled = false;
-        }
-    });
-}
-
+      });
+      document.getElementById(seat.value).classList.add("selected");
+      document.getElementById("seat-number").innerText = `(${selection})`;
+      confirmButton.disabled = false;
+    };
+  });
+};
 
 const toggleFormContent = (event) => {
-    const flightNumber = flightInput.value;
-    console.log(verifyFlightFormat(flightNumber))
-    console.log('toggleFormContent: ', flightNumber);
-    fetch(`/flights/${flightNumber}`)
-        .then(res => res.json())
-        .then(data => {
-            console.log(data);
-        })
-    // TODO: contact the server to get the seating availability
-    //      - only contact the server if the flight number is this format 'SA###'.
-    //      - Do I need to create an error message if the number is not valid?
-    
-    // TODO: Pass the response data to renderSeats to create the appropriate seat-type.
-    renderSeats();
-}
+  const flightNumber = flightInput.value;
+
+  fetch(`/flights/${flightNumber}`)
+    .then((res) => res.json())
+    .then((data) => {
+      if (!data.error) {
+        errorMsg.style.display = 'none';
+        renderSeats(data);
+      } else {
+        errorMsg.style.display = 'block';
+        errorMsg.innerText = errorMessages[data.error];
+      }
+    });
+
+  // TODO: contact the server to get the seating availability
+  //      - Do I need to create an error message if the number is not valid?
+
+  // TODO: Pass the response data to renderSeats to create the appropriate seat-type.
+};
 
 const handleConfirmSeat = (event) => {
-    event.preventDefault();
-    // TODO: everything in here!
-    fetch('/users', {
-        method: 'POST',
-        body: JSON.stringify({
-            'givenName': document.getElementById('givenName').value
-        }),
-        headers: {
-            'Accept': 'application/json',
-            "Content-Type": "application/json"
-        }
-    })
+  event.preventDefault();
+  // TODO: everything in here!
+  fetch("/users", {
+    method: "POST",
+    body: JSON.stringify({
+      givenName: document.getElementById("givenName").value,
+    }),
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+  });
+};
 
-}
-
-flightInput.addEventListener('blur', toggleFormContent);
+flightInput.addEventListener("blur", toggleFormContent);
--- server.js
+++ server.js
@@ -1,33 +1,63 @@
-'use strict';
+"use strict";
 
-const express = require('express');
-const bodyParser = require('body-parser');
-const morgan = require('morgan');
-const { flights } = require('./test-data/flightSeating');
+const express = require("express");
+const bodyParser = require("body-parser");
+const morgan = require("morgan");
+const { flights } = require("./test-data/flightSeating");
+const { formatFlightNumber, validateFlightNumber } = require("./js/helpers");
 
 express()
   .use(function (req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
+    res.header("Access-Control-Allow-Origin", "*");
     res.header(
-      'Access-Control-Allow-Headers',
-      'Origin, X-Requested-With, Content-Type, Accept'
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept"
     );
     next();
   })
-  .use(morgan('dev'))
-  .use(express.static('public'))
+  .use(morgan("dev"))
+  .use(express.static("public"))
   .use(bodyParser.json())
   .use(express.urlencoded({ extended: false }))
-
+  .set("view engine", "ejs")
 
   // endpoints
-.get('/flights/:id', (req, res)=> {
-  const flightNumber = req.params.id
-  console.log(flightNumber)
-  res.status(200).json({
-    status:200,
-    message: 'ok'
+  .get("/seat-select", (req, res) => {
+    const error = req.query.error
+    const errorMessage = error && error==="invalid-format" ? "Invalid format. Should be SA###" : "Flight doesn't exist"
+    res.status(200).render("./pages/seat-select", {
+      status: 200,
+      message: "ok",
+      error: error,
+    });
   })
-})
+
+  .get("/flights/:number", (req, res) => {
+    const flightNumber = formatFlightNumber(req.params.number);
+    try {
+      const isValid = validateFlightNumber(flightNumber);
+      
+      if (isValid) {
+
+        const flight = flights[flightNumber];
+        
+        if (!!flight) {
+          res.status(200).json({status:200, flight: flight});
+        } else {
+          throw {
+            code: 404,
+            type: "do-not-exist",
+          };
+        }
+      } else {
+        throw {
+          code: 400,
+          type: "invalid-format",
+        };
+      }
+    } catch (err) {
+      res.status(err.code).json({status: err.code, error: err.type})
+    }
+  })
 
   .listen(8000, () => console.log(`Listening on port 8000`));
